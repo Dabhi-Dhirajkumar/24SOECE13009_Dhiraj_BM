c8ac948cc3795e48ac13e411b97adcc860b7aae7
Dabhi-Dhirajkumar <dhirajjdabhi1@gmail.com>
2025-07-04 10:33:13 +0530

    type casting

--- Program.cs
+++ Program.cs
@@ -10,6 +10,19 @@ namespace _24SOECE13009_Dhiraj_BM
     {
         static void Main(string[] args)
         {
+            //without mention datda type
+            //var a=10;
+            Console.WriteLine("Enter num 1:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter num 1:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sum;
+            sum = a + b;
+            Console.WriteLine($"sum is {a},{b}:"+sum);
+
+            //Console.Read();
+
+
         }
     }
 }
