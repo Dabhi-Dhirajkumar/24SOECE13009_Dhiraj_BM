545bd9cce5fb251ca15b1c8559b4faa93611f75d
Dabhi-Dhirajkumar <dhirajjdabhi1@gmail.com>
2025-07-04 11:37:56 +0530

    hello_T1

---
+++ Lab_Tutorial_1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24SOECE13009_Dhiraj_BM
+{
+    internal class Lab_Tutorial_1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Console.Read();
+        }
+    }
+}
